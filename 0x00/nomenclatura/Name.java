ae792205b82cd41f8348ec3e2e4e60b4f053cfdd
Pedro H. Deluca <pedrodluk@gmail.com>
2025-10-07 15:16:58 +0000

    Subindo exercicio 0

---
+++ 0x00/nomenclatura/Name.java
@@ -0,0 +1,18 @@
+public class Name {
+    public Date modifiedDate;
+    public List<Testcase> findAllTestcasesByUser(Name.User user){};
+
+    public List<Account> AccountList; 
+
+    public void deleteUser(Name.User user){};
+
+    public URL url;
+
+    public class TableRepresenter{};
+    public void car(){}; 
+
+    public saveUser(){}; createAccount(){}; generateContract(){};
+ 
+    public final int DAYS_IN_YEAR = 360;
+    public final int HOURS_IN_WEEK = 168;
+}
